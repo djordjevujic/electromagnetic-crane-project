315fb1ffb67ca958c1d45c7a6eef5a442a2f2cb4
djordjevujic <djordjevujic@live.com>
2017-09-12 11:12:49 +0200

    Update dcmotor3clickTest.ino

--- dcmotor3clickTest.ino
+++ dcmotor3clickTest.ino
@@ -5,16 +5,23 @@
  *                  5V (electronics)
  *  Connecting pins 
  *  DCMotor3Click       Arduino
- *  SLP-----------------10
+ *  SLP-----------------11
  *  IN1-----------------8
  *  IN2-----------------9
- *  PWM-----------------3
+ *  PWM-----------------10
+ *  
+ *  The base frequency for pins 3, 9, 10, and 11 is 31250 Hz.
+
+ *  divisors for 3 and 11 are 1,8,32,64,128,256,1024
+ *  divsors  for 5, 6, 9 and 10 are 1, 8, 64, 256, 1024
 */
 
-#define SLP_PIN     10
+#define SLP_PIN     11
 #define IN2_PIN     9
 #define IN1_PIN     8
-#define PWM_PIN     3
+#define PWM_PIN     10
+
+#define PWM_FREQ_DIVISOR    8
 
 //this configuration means Stop
 char IN1  = 0;
@@ -74,6 +81,38 @@ void dc_motor3_set_speed(char speed)
     analogWrite(PWM_PIN, speed);
 }
 
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
+
 void setup() {
   // put your setup code here, to run once:
  
@@ -81,6 +120,8 @@ void setup() {
   pinMode(IN1_PIN, OUTPUT);
   pinMode(IN2_PIN, OUTPUT);
   pinMode(PWM_PIN, OUTPUT);
+
+  setPwmFrequency(PWM_PIN, PWM_FREQ_DIVISOR);
 
   dc_motor3_clockwise();
   dc_motor3_set_speed(speed);
