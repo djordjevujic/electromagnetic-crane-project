c97b64f5c3fae5782e99e62136815648fbc0c520
Djordje Vujic <djordjevujic@live.com>
2017-10-05 12:08:39 +0200

    DC works. CW, CCW, Stop TESTED

--- crane_complete_test/crane_complete_test.ino
+++ crane_complete_test/crane_complete_test.ino
@@ -11,27 +11,21 @@
 #define STEPPER_CCW        LOW
 
 //DC motor parameters
-
+#define SLP_PIN     11
+#define IN2_PIN     9
+#define IN1_PIN     8
+#define PWM_PIN     10
+#define PWM_FREQ_DIVISOR    8
 
 #define DEBUG 1
 
 char readByte;
 
-void setup() {
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  #ifdef DEBUG
-    Serial.println("Debuging mode is ON!");
-    Serial.println("Baud rate is 57600");
-  #endif
-  pinMode(ENABLE,OUTPUT);
-  pinMode(STEP,OUTPUT);
-  pinMode(DIR,OUTPUT);
-}
-
+//this configuration means Stop
+char IN1  = 0;
+char IN2  = 0;
+char SLP  = 1;
+char speed = 127;
 
 void stepNow(int totalSteps) {
   Serial.print(totalSteps);
@@ -58,11 +52,123 @@ void stepper_move(int step_dir, int steps)
   stepNow(steps);
 }
 
+
+void dc_motor3_brake()
+{
+  IN1 = IN2 = 1;
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, HIGH);
+}
+
+void dc_motor3_stop()
+{
+  if(SLP != 1)
+  {
+     SLP = 1;
+     digitalWrite(SLP_PIN, HIGH);
+  }
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  IN1 = IN2 = 0;  
+}
+
+void dc_motor3_standby()
+{
+  SLP = 0;
+  digitalWrite(SLP_PIN, LOW);
+}
+
+void dc_motor3_clockwise()
+{
+  if(SLP != 1)
+    SLP = 1;
+  IN1 = 0;
+  IN2 = 1;
+  digitalWrite(SLP_PIN, HIGH);
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);  
+}
+
+void dc_motor3_counter_clockwise()
+{
+  if(SLP != 1)
+      SLP = 1;
+  IN1 = 1;
+  IN2 = 0;
+  digitalWrite(SLP_PIN, HIGH);
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+}
+
+void dc_motor3_set_speed(char speed)
+{
+    analogWrite(PWM_PIN, speed);
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+void setup() {
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  #ifdef DEBUG
+    Serial.println("Debuging mode is ON!");
+    Serial.println("Baud rate is 57600");
+  #endif
+  //Stepper
+  pinMode(ENABLE,OUTPUT);
+  pinMode(STEP,OUTPUT);
+  pinMode(DIR,OUTPUT);
+  //DC motor
+  pinMode(SLP_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+  
+  setPwmFrequency(PWM_PIN, PWM_FREQ_DIVISOR);
+  
+  dc_motor3_stop();
+  dc_motor3_set_speed(speed);
+}
+
 void loop() {
   digitalWrite(ENABLE,LOW);
  
   while(!Serial.available());
-  if((readByte = Serial.read()) == 'a')
+  readByte = Serial.read();
+  Serial.print("Unesena komanda: ");
+  Serial.println(readByte);
+  if(readByte == 'a')
   {
      stepper_move(STEPPER_CW, STEPS_PER_ROTATION/4);
     //walkOneDirection();
@@ -73,4 +179,17 @@ void loop() {
     stepper_move(STEPPER_CCW, STEPS_PER_ROTATION/4);
     Serial.println("Function stepper_move(ccw, steps_per_rot / 4) executed!");
   }
+   else if(readByte == 'w')
+  {
+    dc_motor3_counter_clockwise();
+  }
+  else if(readByte == 'x')
+  {
+    dc_motor3_clockwise();
+  }
+  else if(readByte == 's')
+  {
+    dc_motor3_stop();
+  }
+  
 }
